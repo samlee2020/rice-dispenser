d628f0f919b437f4f0ab6a1aa3fce4395ba4220e
samlee2020 <75598713+samlee2020@users.noreply.github.com>
2022-08-10 22:31:06 -0700

    Add files via upload

---
+++ candy_dispenser.ino
@@ -0,0 +1,67 @@
+#include "SPI.h" 
+#include "MFRC522.h" // 
+#include <ESP32_Servo.h>
+
+Servo myservo;
+const int pinRST = 9;
+const int pinSDA = 5;
+int pinIrSensor = 22;
+MFRC522 mfrc522(pinSDA, pinRST);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(115200);   // Initiate a serial communication
+  pinMode (pinIrSensor, INPUT); // sensor pin INPUT
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Please scan your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Check if the candy is empty
+  int sensorStatus = digitalRead(pinIrSensor); // Set the GPIO as Input
+  if (sensorStatus == 1)
+  {
+    Serial.println("CANDY CHAMBER IS GETTING LOW");
+  }
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "43 36 1A 07") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    myservo.attach(16);
+    myservo.write(180);
+    delay(3000);
+    myservo.detach();
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    delay(3000);
+  }
+} 
